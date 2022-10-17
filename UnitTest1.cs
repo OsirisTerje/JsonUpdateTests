388518d8b0c5c02b3161afd1083f9b39b312f4e7
Terje Sandstrom <terje@hermit.no>
2022-10-17 21:37:20 +0200

    Updated

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -5,21 +5,6 @@ using System.Text.Json.Serialization;
 
 namespace jsonupdate;
 
-public class Tests
-{
-    [SetUp]
-    public void Setup()
-    {
-    }
-
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
-}
-
-
 public class Country
 {
     public int Code { get; set; }
@@ -58,9 +43,9 @@ public class JsonFile
     {
         var countries = new List<Country>
         {
-            new Country(47, "Norway", 5300000),
-            new Country(1, "USA", 350000000),
-            new Country(46, "Sweden", 10500000)
+            new(47, "Norway", 5300000),
+            new(1, "USA", 350000000),
+            new(46, "Sweden", 10500000)
         };
         var json = JsonSerializer.Serialize(countries);
         File.WriteAllText(Path, json);
@@ -80,6 +65,7 @@ public class JsonFile
 
     /// <summary>
     /// This is the pattern for updating a non-record based file, e.f. a json or csv or whatever.
+    /// With a bit of testing code added :-)
     /// </summary>
     [Test]
     public void ThatWeCanUpdateJsonFile()
@@ -88,7 +74,7 @@ public class JsonFile
         var countries = JsonSerializer.Deserialize<Country[]>(jsonIn);   // Note:  Here it is just an array, we could also make it into a list by adding ToList() after it. 
         
         // Let us change the population of USA to 332,403,650 (2022 numbers)
-        var usa = countries.Single(o => o.Code == 1);  // Note that usa in this case is just a Reference to the element in the array, so when we change it on the next line, we're actually changing it IN the array too
+        var usa = countries!.Single(o => o.Code == 1);  // Note that usa in this case is just a Reference to the element in the array, so when we change it on the next line, we're actually changing it IN the array too
         usa.Population = 332403650;
         
         // Write it back
@@ -99,7 +85,7 @@ public class JsonFile
         jsonIn = File.ReadAllText(Path);
         var countriesUpdated = JsonSerializer.Deserialize<Country[]>(jsonIn);
 
-        Assert.That(countriesUpdated.Length,Is.EqualTo(countries.Length),"Some record was added during this operation, it should not.....");
+        Assert.That(countriesUpdated!.Length,Is.EqualTo(countries!.Length),"Some record was added during this operation, it should not.....");
         var usaUpdated = countriesUpdated.Single(o => o.Code == 1);
         Assert.That(usaUpdated.Population, Is.EqualTo(usa.Population));
     }
